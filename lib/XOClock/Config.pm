0b1edaa787d798e0019d0beba9daeeb8dc0aff26
Kenta Sato <karupa@cpan.org>
2012-01-22 21:15:55 +0900

    support multi cconfig file.

--- lib/XOClock/Config.pm
+++ lib/XOClock/Config.pm
@@ -7,52 +7,183 @@ use utf8;
 our $VERSION = '0.01';
 
 use Class::Accessor::Lite (
-    ro  => [qw/max_workers worker/],
+    ro  => [qw/max_workers/],
+    rw  => [qw/worker marged_config/],
 );
 use Data::Validator;
 use YAML::Syck ();
 use Log::Minimal;
 
-sub config_rule {
-    state $rule = Data::Validator->new(
-        max_workers => +{ isa => 'Int' },
-        worker      => +{ isa => 'HashRef[Str]' },
-    );
-
-    return $rule;
-}
-
-sub new {
-    my $class  = shift;
-    my $config = $class->config_rule->validate(@_);
-
-    bless(+{ %$config } => $class)->init;
-}
-
-sub init {
-    my $self = shift;
-
-    return $self;
-}
-
 sub load {
-    my $class  = shift;
-    my $config = $class->_load(@_);
+    state $rule = Data::Validator->new(
+        file => +{ isa => 'Str' },
+        type => +{ isa => 'Str', default => 'main' }
+    )->with(qw/Method/);
+    my($class, $arg) = $rule->validate(@_);
+    my $config = $class->_load($arg);
 
-    $class->new($config);
+    return $class->new(
+        validated => 1,
+        config    => $config,
+    );
 }
 
 sub _load {
     state $rule = Data::Validator->new(
-        file => +{ isa => 'Str' }
+        file => +{ isa => 'Str' },
+        type => +{ isa => 'Str' },
     )->with(qw/Method/);
     my($class, $arg) = $rule->validate(@_);
 
     return do {
         local $YAML::Syck::ImplicitUnicode = 1;
         infof(q{load config from '%s'.}, $arg->{file});
-        YAML::Syck::LoadFile($arg->{file});
+        $class->config_validate(
+            config => scalar(YAML::Syck::LoadFile($arg->{file})),
+            file   => $arg->{file},
+            type   => $arg->{type},
+        )
     };
+}
+
+sub new {
+    state $rule = Data::Validator->new(
+        config => +{ isa => 'HashRef' },
+        type   => +{ isa => 'Str' },
+        file   => +{ isa => 'Str' },
+        validated => +{ isa => 'Bool', default => 0, xor => [qw/type file/] }
+    )->with(qw/Method/);
+    my($class, $arg) = $rule->validate(@_);
+    my $config = $arg->{validated} ?
+        $arg->{config}:
+        $class->config_validate($arg);
+
+    bless(+{ %$config } => $class)->init;
+}
+
+sub init {
+    my $self = shift;
+    state $child_rule = Data::Validator->new($self->child_config_rule);
+
+    $self->marged_config(+{});
+    if (exists $self->{config_file}) {
+        $self->load_child_config(@{ delete $self->{config_file} });
+    }
+
+    return $self;
+}
+
+sub load_child_config {
+    my($self, @files) = @_;
+
+    foreach my $file (@files) {
+        unless (exists $self->marged_config->{$file}) {
+            my $config = $self->_load(file => $file, type => 'child');
+            $self->marged_config->{$file} = $config;
+            $self->merge($config);
+        }
+    }
+}
+
+sub merge {
+    my($self, $config) = @_;
+
+    foreach my $key (keys %$config) {
+        given ($key) {
+            when ('worker') {
+                $self->merge_worker($config->{worker});
+            }
+            when ('config_file') {
+                $self->load_child_config(@{ $config->{config_file} });
+            }
+            default {
+                require Carp;
+                Carp::croak "Unknown config: $key";
+            }
+        }
+    }
+}
+
+sub merge_worker {
+    my($self, $worker) = @_;
+
+    foreach my $name (keys %$worker) {
+        if ( exists $self->worker->{$name} ) {
+            require Carp;
+            Carp::croak "duplicate worker: $name";
+        }
+    }
+
+    $self->worker(
+        +{
+            %{ $self->worker },
+            %$worker,
+        }
+    );
+}
+
+sub config_validate {
+    state $rule = Data::Validator->new(
+        config => +{ isa => 'HashRef' },
+        type   => +{ isa => 'Str' },
+        file   => +{ isa => 'Str' },
+    )->with(qw/Method/);
+    my($class, $arg) = $rule->validate(@_);
+
+    my $config_rule;
+    given ($arg->{type}) {
+        when ('main') {
+            state $_config_rule = Data::Validator->new($class->config_rule)->with(qw/NoThrow/);
+            $config_rule = $_config_rule;
+        }
+        when ('child') {
+            state $_config_rule = Data::Validator->new($class->child_config_rule)->with(qw/NoThrow/);
+            $config_rule = $_config_rule;
+        }
+        default {
+            require Carp;
+            Carp::croak "Unknown type: $arg->{type}";
+        }
+    }
+
+    my $config = $config_rule->validate($arg->{config});
+    if ($config_rule->has_errors) {
+        my $errors = $config_rule->clear_errors;
+
+        critf('config file validate failed. file: "%s"', $arg->{file});
+        foreach my $e (@$errors) {
+            critf(q{Error name: '%s', type: '%s', message: '%s'.}, $e->{name}, $e->{type}, $e->{message});
+        }
+
+        require Carp;
+        Carp::croak 'config file validate failed.';
+    }
+
+    return $config;
+}
+
+sub common_rule {
+    return (
+     worker      => +{ isa => 'HashRef[Str]' },
+     config_file => +{ isa => 'ArrayRef[Str]', optional => 1 },
+    )
+}
+
+sub config_rule {
+    my $class = shift;
+
+    return (
+        max_workers => +{ isa => 'Int' },
+        $class->common_rule,
+    );
+}
+
+sub child_config_rule {
+    my $class = shift;
+
+    return (
+        $class->common_rule,
+    );
 }
 
 1;
